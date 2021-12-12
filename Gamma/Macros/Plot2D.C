a0ce3f1c4cf79ed432cac355d8a732d9ef2fcbbc
Aurora Leso <80251912+auroraleso@users.noreply.github.com>
2021-12-12 08:59:35 +0100

    edited macros

---
+++ Gamma/Macros/Plot2D.C
@@ -0,0 +1,169 @@
+#include "gethisto.C"
+#include <vector>
+#include <fstream>
+#include "../../Compton/macros/RootStyle.cc"
+
+
+void plot(const string name_file) {
+	// variables
+
+    
+	slimport_data_t indata1,indata2,indata3,indata4;
+	TFile *infile = new TFile(name_file.c_str());
+	TTree *intree = (TTree*)infile->Get("acq_tree_0");
+
+	TBranch *inbranch1 = intree->GetBranch("acq_ch0");
+    TBranch *inbranch2 = intree->GetBranch("acq_ch1");
+    TBranch *inbranch3 = intree->GetBranch("acq_ch2");
+    TBranch *inbranch4 = intree->GetBranch("acq_ch3");
+
+	inbranch1->SetAddress(&indata1.timetag);
+    inbranch2->SetAddress(&indata2.timetag);
+    inbranch3->SetAddress(&indata3.timetag);
+    inbranch4->SetAddress(&indata4.timetag);
+
+    TH2* h1 = new TH2F("h1", "Detector 1",400,0,4000,400,0,1400);
+    TH2* h2 = new TH2F("h2", "Detector 2",400,0,4000,400,0,1400);
+    TH2* h3 = new TH2F("h3", "Detector 3",400,0,4000,400,0,1400);
+
+    TH2* h_fil = new TH2F("h3", "Filtered spectrum",300,0,4000,400,0,1400);
+
+
+        
+
+	// histogram filling
+    double entry1,entry2,entry3,entry4;
+    
+	for (int i=0; i<inbranch1->GetEntries(); i++) {
+        inbranch2->GetEntry(i);
+        entry2 =  -21.7267 + indata2.qlong*0.114383  ;
+		inbranch1->GetEntry(i);
+        entry1 = -17.7045 + indata1.qlong*0.115652;
+        inbranch3->GetEntry(i);
+        entry3 = -22.9221 + indata3.qlong*0.110317;
+        inbranch4->GetEntry(i);
+        entry4 = -0.0201002 + 0.0116122*indata4.qlong;
+
+        double sum = entry1+entry2+entry3;
+
+        h1->Fill(sum,entry1);
+        h2->Fill(sum,entry2);
+        h3->Fill(sum,entry3);
+
+
+       
+		if ((entry1 + entry2 + entry3) < 1124.2 && (entry1 + entry2 + entry3) > 919.8) {
+        if ( (entry1 < 450 && entry1 > 0) && (entry2 < 450 && entry2 > 0) && (entry3 < 450 && entry3 > 0)){
+
+		h_fil->Fill(sum,entry2);
+          }
+        }
+    }
+
+
+
+   //gStyle->SetPalette(58);
+    //gStyle->SetPalette(kColorPrintableOnGrey);
+    //gStyle->SetNumberContours(70);
+    gStyle->SetPalette(kBird);
+        //TColor::InvertPalette();
+    //TCanvas* c = new TCanvas("c", "canvas", 800, 800);
+    //c->Divide(2, 2, 0.0005, 0.0005);
+    //c->cd(1);
+    TCanvas* c1 = new TCanvas("c1","D1",1080,1020);
+    gPad->SetLeftMargin(0.12);
+    gStyle->SetOptStat(0000);
+    gStyle->SetTitleY(0.975);
+    h1->SetMinimum(0);
+    h1->SetMaximum(30);
+    h1->GetXaxis()->SetTitle("E_{1} + E_{2} + E_{3} [keV]");
+    h1->GetXaxis()->SetLabelOffset(0.01);
+    h1->GetXaxis()->SetLabelSize(0.035);
+    h1->GetXaxis()->SetTitleSize(0.04);
+    h1->GetXaxis()->SetTitleOffset(1.15);
+    h1->GetYaxis()->SetTitle("E_{1} [keV]");
+    h1->GetYaxis()->SetLabelOffset(0.008);
+    h1->GetYaxis()->SetLabelSize(0.035);
+    h1->GetYaxis()->SetTitleSize(0.04);
+    h1->GetYaxis()->SetTitleOffset(1.2);
+    h1->GetXaxis()->SetRangeUser(0, 3000);
+    h1->GetYaxis()->SetRangeUser(0, 1400);
+    h1->SetLineWidth(1);
+    h1->Draw("COLZ2");
+    TLine *l1 = new TLine(1022,0,1022,1400);
+    TLine *l2 = new TLine(0,304,3000,304);
+    l1->Draw("same");
+    l2->Draw("same");
+   c1->Update();
+
+    TCanvas* c2 = new TCanvas("c2","D2",1080,1020);
+    //c->cd(2);
+
+    gPad->SetLeftMargin(0.12);
+    h2->SetMinimum(0);
+    h2->SetMaximum(30);
+    h2->GetXaxis()->SetTitle("E_{1} + E_{2} + E_{3} [keV]");
+    h2->GetXaxis()->SetLabelOffset(0.01);
+    h2->GetXaxis()->SetLabelSize(0.035);
+    h2->GetXaxis()->SetTitleSize(0.04);
+    h2->GetXaxis()->SetTitleOffset(1.15);
+    h2->GetYaxis()->SetTitle("E_{2} [keV]");
+    h2->GetYaxis()->SetLabelOffset(0.008);
+    h2->GetYaxis()->SetLabelSize(0.035);
+    h2->GetYaxis()->SetTitleSize(0.04);
+    h2->GetYaxis()->SetTitleOffset(1.2);
+    h2->GetXaxis()->SetRangeUser(0, 3000);
+    h2->GetYaxis()->SetRangeUser(0, 1400);
+    h2->SetLineWidth(1);
+    h2->Draw("COLZ2");
+    l1->Draw("same");
+    l2->Draw("same");
+
+
+   
+
+    TCanvas* c3 = new TCanvas("c3","D3",1080,1020);
+    //c->cd(3);
+    gPad->SetLeftMargin(0.12);
+    h3->SetMinimum(0);
+    h3->SetMaximum(30);
+    h3->GetXaxis()->SetTitle("E_{1} + E_{2} + E_{3} [keV]");
+    h3->GetXaxis()->SetLabelOffset(0.01);
+    h3->GetXaxis()->SetLabelSize(0.035);
+    h3->GetXaxis()->SetTitleSize(0.04);
+    h3->GetXaxis()->SetTitleOffset(1.15);
+    h3->GetYaxis()->SetTitle("E_{3} [keV]");
+    h3->GetYaxis()->SetLabelOffset(0.008);
+    h3->GetYaxis()->SetLabelSize(0.035);
+    h3->GetYaxis()->SetTitleSize(0.04);
+    h3->GetYaxis()->SetTitleOffset(1.2);
+    h3->GetXaxis()->SetRangeUser(0, 3000);
+    h3->GetYaxis()->SetRangeUser(0, 1400);
+    h3->SetLineWidth(1);
+    h3->Draw("COLZ2");
+    l1->Draw("same");
+    l2->Draw("same");
+    
+    TCanvas* c4 = new TCanvas("c4","fil",1080,1020);
+    //c->cd(4);
+    gPad->SetLeftMargin(0.12);
+    //h_fil->SetMinimum(0);
+    h_fil->SetMaximum(25);
+    h_fil->GetXaxis()->SetTitle("E_{1} + E_{2} + E_{3} [keV]");
+    h_fil->GetXaxis()->SetLabelOffset(0.01);
+    h_fil->GetXaxis()->SetLabelSize(0.035);
+    h_fil->GetXaxis()->SetTitleSize(0.04);
+    h_fil->GetXaxis()->SetTitleOffset(1.15);
+    h_fil->GetYaxis()->SetTitle("E_{1} [keV]");
+    h_fil->GetYaxis()->SetLabelOffset(0.008);
+    h_fil->GetYaxis()->SetLabelSize(0.035);
+    h_fil->GetYaxis()->SetTitleSize(0.04);
+    h_fil->GetYaxis()->SetTitleOffset(1.2);
+    h_fil->GetXaxis()->SetRangeUser(700, 1300);
+    h_fil->GetYaxis()->SetRangeUser(100, 520);
+    h_fil->SetLineWidth(1);
+    h_fil->Draw("COLZ2");
+
+   
+}
+
---
+++ Gamma/Macros/test.C
@@ -0,0 +1,124 @@
+
+using namespace std;
+
+//C++ LIBRARIES
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cmath>
+//ROOT LIBRARIES
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TMath.h"
+
+double_t myFit(double_t *x, double_t *par) 
+{
+  double_t fitval = x[0]/(par[0]*x[0] + 1);
+  return fitval;
+}
+
+
+void plot()
+{   
+
+
+    vector<Double_t> x = {10435,12441,12521,6729,4432,42};
+    vector<Double_t> y ={3810.13333333333,6542.93333333333,4959.0303030303,6540.8,3530.66666666667,39.4666666666667};
+
+    vector<Double_t> x2 = {42,4432,6729,10435,12441,12521};
+    vector<Double_t> y2 ={39.4666666666667,3530.66666666667,6540.8,3810.13333333333,6542.93333333333,4959.0303030303};
+    vector<Double_t> ey2 ={0.8110350040397,7.6710132606093,10.440945040241,7.9688281586580,10.442647599574,9.09123231748617};
+
+    vector<Double_t> err = {1.9310357239042 ,0.173082429165373, 0.155163397833868, 0.076366345554941, 0.083937365160147, 0.0726078773060153};
+
+    vector<Double_t> loss;
+    for(int i=0; i<x2.size();i++) {
+        double l = 100 *(x2[i] - y2[i])/x2[i];
+        loss.push_back(l);
+        cout << loss[i] << endl;
+    }
+
+
+    TGraphErrors *graph0 = new TGraphErrors(1, &x2[0], &loss[0], 0,&err[0]);
+    TGraphErrors *graph1 = new TGraphErrors(1, &x2[1], &loss[1], 0,&err[1]);
+    TGraphErrors *graph2 = new TGraphErrors(1, &x2[2], &loss[2], 0,&err[2]);
+    TGraphErrors *graph3 = new TGraphErrors(1, &x2[3], &loss[3], 0,&err[3]);
+    TGraphErrors *graph4 = new TGraphErrors(1, &x2[4], &loss[4], 0,&err[4]);
+    TGraphErrors *graph5 = new TGraphErrors(1, &x2[5], &loss[5], 0,&err[5]);
+
+  
+    
+    TGraphErrors *graph6 = new TGraphErrors(6, &x2[0], &loss[0],0,&err[0]);
+    TCanvas* c2 = new TCanvas("c2","Dead time analysis",1080,1020);
+    
+
+    TMultiGraph* mg = new TMultiGraph();
+    mg->Add(graph0);
+    mg->Add(graph1);
+    mg->Add(graph2);
+    mg->Add(graph3);
+    mg->Add(graph4);
+    mg->Add(graph5);
+    mg->Add(graph6);
+  
+    graph0->SetMarkerColor(632);
+    graph0->SetMarkerStyle(8);
+    graph0->SetMarkerSize(1);
+
+    graph1->SetMarkerColor(800);
+    graph1->SetMarkerStyle(8);
+    graph1->SetMarkerSize(1);
+
+    graph2->SetMarkerColor(kViolet);
+    graph2->SetMarkerStyle(8);
+    graph2->SetMarkerSize(1);
+
+    graph3->SetMarkerColor(416);
+    graph3->SetMarkerStyle(8);
+    graph3->SetMarkerSize(1);
+
+    graph4->SetMarkerColor(432);
+    graph4->SetMarkerStyle(8);
+    graph4->SetMarkerSize(1);
+
+    graph5->SetMarkerColor(kBlue);
+    graph5->SetMarkerStyle(8);
+    graph5->SetMarkerSize(1);
+
+    graph6->SetLineStyle(3);
+	graph6->SetLineWidth(1);
+
+    mg->Draw("aepl");
+   
+
+    gPad->SetLeftMargin(0.12);
+    gStyle->SetTitleY(0.975);
+    mg->SetTitle("Dead time analysis");
+    mg->GetYaxis()->SetTitle("Lost events [%]");
+    mg->GetXaxis()->SetTitle("Rate Scaler [Hz]");
+    mg->GetXaxis()->SetLabelOffset(0.01);
+    mg->GetXaxis()->SetLabelSize(0.035);
+    mg->GetXaxis()->SetTitleSize(0.04);
+    mg->GetXaxis()->SetTitleOffset(1.15);
+    mg->GetYaxis()->SetLabelOffset(0.008);
+    mg->GetYaxis()->SetLabelSize(0.04);
+    mg->GetYaxis()->SetTitleSize(0.04);
+    mg->GetYaxis()->SetTitleOffset(1.);
+
+
+    auto legend = new TLegend(0.1,0.7,0.48,0.9);
+    legend->AddEntry(graph3,"D1","ep");
+    legend->AddEntry(graph5,"D2","ep");
+    legend->AddEntry(graph4,"D3","ep");
+    legend->AddEntry(graph2,"D4","ep");
+    legend->AddEntry(graph1,"D1 & D2","ep");
+    legend->AddEntry(graph0,"D1 & D2 & D4","ep");
+    legend->SetNColumns(2);
+    legend->Draw("same");
+
+
+  
+
+}
