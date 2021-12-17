cc7b3018017bf95f057fdbd90c3c53ec97246510
Aurora Leso <80251912+auroraleso@users.noreply.github.com>
2021-12-17 16:42:59 +0100

    added gaussian fits and efficiencies calcultaions

--- Gamma/macros/SubtractHisto.cc
+++ Gamma/macros/SubtractHisto.cc
@@ -5,16 +5,18 @@
 /*requested files: histogramOfData.root, histogramOfBackground.root, channel*/
 using namespace std;
 
-void subtract(string dataFileName, string bgFileName, short chan)
+void subtract(string dataFileName, string bgFileName, short chan, string outfilename)
 {
-    set_root_style(0);
+    set_root_style(1);
 
     /*Reading histogram file from the Root file*/
 
     //data
+
     TFile *dataFile = new TFile(dataFileName.c_str());
     TH1F *hdata = (TH1F*)dataFile->Get(Form("ch%i",chan));
     //background
+    double conversion_factor_BG = 10./30;
     TFile *bgFile = new TFile(bgFileName.c_str());
     TH1F *hbg = (TH1F*)bgFile->Get(Form("ch%i",chan));
 
@@ -24,7 +26,7 @@ void subtract(string dataFileName, string bgFileName, short chan)
 
     for (int i = 1; i<=hbg->GetXaxis()->GetNbins(); i++) 
     {
-        temp = 2 * hbg->GetBinContent(i); //need to find the factor of conversion for bg
+        temp = conversion_factor_BG * hbg->GetBinContent(i); //need to find the factor of conversion for bg
         hbgConverted->SetBinContent(i, temp);
     }
 
@@ -32,22 +34,18 @@ void subtract(string dataFileName, string bgFileName, short chan)
     double a;
     double b;
 
-    //different calibrations according to the parameters
-    if (chan == 0)
+    //different calibrations according to the parameters needed for the 2 detectors
+    if (chan == 1) //HPGe
     {
-        a = -6.59056;
-        b = 0.063135;
+        a =  -0.019234;
+        b = 0.148907;
     }
-    else if (chan == 1)
+    else if (chan == 0) //NaI
     {
-        a = -7.25507;
-        b = 0.0547132;
+        a = -8.97259;
+        b = 0.0878097;
     }
-    else 
-    {
-        a = -10.8547;
-        b = 0.0607387;
-    }
+
 
     //calibrating x-axis for data spectrum
     int max_binData = hdata->GetNbinsX(); 
@@ -61,7 +59,7 @@ void subtract(string dataFileName, string bgFileName, short chan)
 
     //subtracting bin-to-bin the histogram of bg from the data one
     hdata->Add(hbgConverted, -1.);
-    hdata->GetYaxis()->SetRangeUser(0, 201);
+    //hdata->GetYaxis()->SetRangeUser(0, 201);
     hdata->GetXaxis()->SetRangeUser(0, 600);
 
     float w;
@@ -69,16 +67,20 @@ void subtract(string dataFileName, string bgFileName, short chan)
     hdata->GetXaxis()->SetTitle("Energy [keV]");
     hdata->GetYaxis()->SetTitle(Form("Counts/%0.1f keV",w));
     hdata->SetStats(kFALSE);
-    hdata->SetTitle("Background subtracted Detector spectrum");
+    hdata->SetTitle("Background subtracted spectrum");
     hdata->GetYaxis()->SetTitleOffset(1.4);
     hdata->Draw();
     hdata->Draw();
 
-
-
+  
+    //saving outfile
+    TFile * outfile = new TFile(outfilename.c_str(), "RECREATE");
+	hdata->Write();
+    cout<<"num of entries in the whole histogram:"<<hdata->GetEntries()<<endl;
+	outfile->Close();
     /*We can now fit the 256 keV gaussian*/
 
-    TF1 *fit1 = new TF1("g1","gaus(0)+pol1(3)", 160, 250);
+    /*TF1 *fit1 = new TF1("g1","gaus(0)+pol1(3)", 160, 250);
 
     fit1->SetLineStyle(1);
 	fit1->SetLineWidth(3);
@@ -101,7 +103,7 @@ void subtract(string dataFileName, string bgFileName, short chan)
     h1->SetFillColorAlpha(kAzure-9,1);
     h1->GetXaxis()->SetRangeUser(160,250);
     h1->Draw("SAME");
-    /* Now, in order to get the counts under the gaussian fit */
+    // Now, in order to get the counts under the gaussian fit 
 
     double counts = (fit1->Integral(160,250))/hdata->GetBinWidth(0);
     std::cout << "counts under peak: " << counts << std::endl;
@@ -118,17 +120,9 @@ void subtract(string dataFileName, string bgFileName, short chan)
     TText *t = pt->GetLineWith("3835");
     t->SetTextColor(kBlue);
     TText *t2 = pt->GetLineWith("keV");
-    t2->SetTextColor(kBlue);
+    t2->SetTextColor(kBlue);*/
  
 }
 
 
-/*
-                        Fit result of gaussian
-    1  p0           1.24373e+02   6.76003e+00   2.15600e-02  -3.16930e-05
-    2  p1           2.07696e+02   9.56076e-01   4.09354e-03  -7.93442e-04
-    3  p2          -1.99514e+01   1.65372e+00   3.08414e-03   2.52045e-04
-    4  p3           1.28123e+02   1.69099e+01   1.08323e-02  -8.55828e-05
-    5  p4          -4.87374e-01   8.42461e-02   5.13017e-05  -2.47612e-02
 
-*/
